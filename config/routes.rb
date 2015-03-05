a8e2e44c941b5cf52fb737a92f31040a89e03733
NickNacz <nick@nacz.com>
2015-03-04 22:11:17 -0800

    Add User show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   resources :pins
 
   devise_for :users
+  get 'users/:id' => 'users#show', as: :user
   get 'about' => 'pages#about'
+
 
   root 'pins#index'
  
